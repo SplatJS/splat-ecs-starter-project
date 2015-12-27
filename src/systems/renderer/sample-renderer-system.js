92ec3d9f5b5297ffb59c67f5c8534df349735623
Eric Lathrop <eric@ericlathrop.com>
2015-12-27 00:57:18 -0500

    Use built-in systems

--- src/systems/renderer/sample-renderer-system.js
+++ src/systems/renderer/sample-renderer-system.js
@@ -1,11 +1,10 @@
 "use strict";
 
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-	data.entities.registerSearch("renderPlayer", ["position", "size"]);
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
 		var position = data.entities.get(entity, "position");
 		var size = data.entities.get(entity, "size");
-		context.fillStyle = "yellow";
+		context.fillStyle = "black";
 		context.fillRect(position.x, position.y, size.width, size.height);
-	}, "renderPlayer");
+	}, "camera");
 };
---
+++ src/systems/simulation/sample-simulation-system.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+	}, "player");
+};
