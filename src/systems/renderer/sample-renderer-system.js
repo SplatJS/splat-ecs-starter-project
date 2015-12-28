b07eb0cafef7ba6f8547537a569e26f4ca380d2b
Eric Lathrop <eric@ericlathrop.com>
2015-12-28 14:39:39 -0500

    Rename "data" to "game" in systems for clarity

--- src/systems/renderer/sample-renderer-system.js
+++ src/systems/renderer/sample-renderer-system.js
@@ -1,10 +1,10 @@
 "use strict";
 
-module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-	data.entities.registerSearch("sampleRendererSystem", ["camera", "position", "size"]);
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
+	game.entities.registerSearch("sampleRendererSystem", ["camera", "position", "size"]);
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
-		var position = data.entities.get(entity, "position");
-		var size = data.entities.get(entity, "size");
+		var position = game.entities.get(entity, "position");
+		var size = game.entities.get(entity, "size");
 		context.fillStyle = "black";
 		context.fillRect(position.x, position.y, size.width, size.height);
 	}, "sampleRendererSystem");
