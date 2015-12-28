7eb522b3cd72bb584d7b053cde79b9ca08f0ec22
Eric Lathrop <eric@ericlathrop.com>
2015-12-28 13:34:17 -0500

    Fix crash when camera doesn't have size

--- src/systems/renderer/sample-renderer-system.js
+++ src/systems/renderer/sample-renderer-system.js
@@ -1,10 +1,11 @@
 "use strict";
 
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+	data.entities.registerSearch("sampleRendererSystem", ["camera", "position", "size"]);
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
 		var position = data.entities.get(entity, "position");
 		var size = data.entities.get(entity, "size");
 		context.fillStyle = "black";
 		context.fillRect(position.x, position.y, size.width, size.height);
-	}, "camera");
+	}, "sampleRendererSystem");
 };
