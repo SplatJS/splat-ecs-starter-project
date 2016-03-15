d174f1d07eec7c165c5a4b7c42455cfadac74035
Eric Lathrop <eric@ericlathrop.com>
2016-03-14 17:25:12 -0700

    Add Electron build files

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,39 @@ var webpack = require("webpack");
 var environment = process.env["NODE_ENV"] || "development";
 
 module.exports = {
-	entry: "./src/game",
-	output: {
-		path: __dirname + "/build",
-		filename: "index.js"
-	},
-	module: {
-		preLoaders: [
-			{ test: /\.(js|json)$/, exclude: /node_modules/, loader: "eslint-loader" }
-		],
-		loaders: [
-			{ test: /\.json$/, loader: "json" },
-			{
-				test: /src\/images\/.*\.(jpe?g|png|gif|svg)$/i,
-				loaders: [
-					"file?hash=sha512&digest=hex&name=images/[name].[ext]",
-					"image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false"
-				]
-			},
-			{
-				test: /src\/sounds\/.*\.(mp3|ogg|wav)$/i,
-				loader: "file?hash=sha512&digest=hex&name=sounds/[name].[ext]"
-			},
-			{
-				test: /src\/index.html$/i,
-				loader: "file?hash=sha512&digest=hex&name=[name].[ext]"
-			}
-		]
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			__PRODUCTION__: environment === "production",
-			__TEST__: environment === "test",
-			__DEVELOPMENT__: environment === "development"
-		})
-	]
+  entry: "./src/game",
+  output: {
+    path: __dirname + "/build/html",
+    filename: "index.js"
+  },
+  module: {
+    preLoaders: [
+      { test: /\.(js|json)$/, exclude: /node_modules/, loader: "eslint-loader" }
+    ],
+    loaders: [
+      { test: /\.json$/, loader: "json" },
+      {
+        test: /src\/images\/.*\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          "file?hash=sha512&digest=hex&name=images/[name].[ext]"
+          //"image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false"
+        ]
+      },
+      {
+        test: /src\/sounds\/.*\.(mp3|ogg|wav)$/i,
+        loader: "file?hash=sha512&digest=hex&name=sounds/[name].[ext]"
+      },
+      {
+        test: /src\/index.html$/i,
+        loader: "file?hash=sha512&digest=hex&name=[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __PRODUCTION__: environment === "production",
+      __TEST__: environment === "test",
+      __DEVELOPMENT__: environment === "development"
+    })
+  ]
 };
