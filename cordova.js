bd8c7e7e80ad3bacfb098d50fca3ef830c32e419
Barryrowe <barryrowe@gmail.com>
2016-10-27 19:52:44 -0400

    Remove ios example and old cordova script
    This removes the ios platform target for now, as it fails on mac osx due
    to a child_process error that I haven't tracked down yet. This doesn't
    setup the config.xml to include the icon and splash image values yet.
    I'm still thinking about the best way to do that. Having to configure
    every size in the package.json feels cumbersome. I like the idea of
    configuring image root folders for icon/splash per platform and the
    script adding the config nodes in the config.xml to point to expected
    file names. The docs could then say what images are expected for each
    platform and the devlopers would just put those images at those sizes
    into the respective folders for packaging. This is how Ionic does it,
    though they provide a CLI tool to generate the images as well.

--- cordova.js
+++ cordova.js
@@ -10,8 +10,7 @@ var cordovaExecutablePath = path.resolve(__dirname, "./node_modules/cordova/bin/
 if(!appMetadata){
   console.error("Your package.json does not have a properly formatted 'appMetadata' property. Unable to package cordova app(s)");
 }else{  
-  runChildProcess("npm run clean", "Unable to Clean Project :(")
-    .then(() => createCordovaProject(cordovaExecutablePath, appMetadata.bundleId, appMetadata.appName))    
+    createCordovaProject(cordovaExecutablePath, appMetadata.bundleId, appMetadata.appName)
     .then((success) => console.log("Project Created"))
     .then(() => process.chdir(`./${outDir}`))
     .then(addPlatforms)
@@ -28,16 +27,16 @@ function runChildProcess(command, failureMessage = "Failed to Run Child Process"
   return new Promise((resolve, reject) => {             
     childProcess = executeCommand(command);
     childProcess.stdout.on('data', function(data) {
-      console.log(data);
+      process.stdout.write(data);
     });
     childProcess.stderr.on('data', function(data) {
-        console.error(data);
+        process.stderr.write(data);
     });
-    childProcess.on('close', function(code) {        
+    childProcess.on('close', function(code, signal) {        
         if(code === 0){
           resolve(true);
         }else{
-          reject(new Error(`${failureMessage} Result Code ${code}`));
+          reject(new Error(`${failureMessage} Result Code ${code}. Signal ${signal}`));
         }
     });
   });
